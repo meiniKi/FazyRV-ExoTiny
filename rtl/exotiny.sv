da7001f0655aeeb1fbfa60ca6642f448bf944820
matztron <muschmatthias@gmail.com>
2025-08-14 22:34:15 +0200

    Added soc_test for watchdog

--- rtl/exotiny.sv
+++ rtl/exotiny.sv
@@ -106,7 +106,6 @@ logic [31:0]  wb_spi_wdat;
 logic         sel_rom_ram;
 logic         sel_wdg;
 logic         sel_mem;
-logic         sel_wdg;
 logic         sel_regs;
 logic         sel_spi;
 
@@ -131,11 +130,11 @@ assign wdg_res_n  = rst_in & wdg_res_en_n;
 // wdg_res_en_n is gated by ~gpo[1] (inverted as init by 0)
 assign core_res_n = rst_in & (core_res_en_n | ~gpo[2]);
 
-assign ccx_rs_a_o = ccx_rs_a;
-assign ccx_rs_b_o = ccx_rs_b;
-assign ccx_req_o  = ccx_req;
-assign ccx_res    = ccx_res_i;
-assign ccx_resp   = ccx_resp_i;
+//assign ccx_rs_a_o = ccx_rs_a;
+//assign ccx_rs_b_o = ccx_rs_b;
+//assign ccx_req_o  = ccx_req;
+//assign ccx_res    = ccx_res_i;
+//assign ccx_resp   = ccx_resp_i;
 
 // we don't have enough io, thus
 // we use gpo[1] to mux whether soft cs or by peripheral
@@ -296,16 +295,16 @@ fazyrv_top #(
 // wdg
 wdg_top #(
   // Wishbone
-  .REG_ADDRESS_WIDTH    (  32             ), // <- TODO
+  .REG_ADDRESS_WIDTH    (  32             ),
   .REG_PRE_DECODE       (  0              ),
-  .REG_BASE_ADDRESS     (  32'h8000_0000  ), // <- TODO
+  .REG_BASE_ADDRESS     (  32'h8000_0000  ),
   .REG_ERROR_STATUS     (  0              ),
   .REG_DEFAULT_READ     (  0              ),
   .REG_INSERT_SLICER    (  0              ),
-  .REG_USE_STALLS       (  0              ), // idk?
+  .REG_USE_STALLS       (  0              ),
   .WB_DATA_WIDTH        ( 32              ),
   .WDG_PRECLKDIV_WIDTH  ( 20              ),
-  .WDG_TICK_BIT         ( 19              ) // can be set from 0 up to WDG_PRECLKDIV_WIDTH-1
+  .WDG_TICK_BIT         ( 2              ) // can be set from 0 up to WDG_PRECLKDIV_WIDTH-1 // !!! ALERT: MAKE SURE BEFORE TAPEOUT THIS IS 19!!!
 ) i_wdg_top (
   .clk                  ( clk_i       ),
   .res_n                ( wdg_res_n   ),
@@ -322,8 +321,8 @@ wdg_top #(
   .o_wb_rty             ( /* NC */    ),
   .o_wb_dat             ( wb_wdg_rdat ),
   // ---
-  .o_irq1                (),              // NC stage 1 watchdog timeout
-  .o_irq2                ( wdg_to     )   //    stage 2 watchdog timeout //TODO make safer
+  .o_irq1                ( wdg_to     ),  //    stage 1 watchdog timeout
+  .o_irq2                ( /* NC */   )   // NC stage 2 watchdog timeout
 );
 
 reset_ctrl #(
